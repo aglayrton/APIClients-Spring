87070846eb702ce8a21887513d351a7bcc3e8e0a
AglayrtonUECE <86808209+AglayrtonUECE@users.noreply.github.com>
2021-07-22 09:37:12 -0300

    Projeto iniciado

---
+++ src/main/java/com/devsuperiorcapitulo1/DevsuperiorCapitulo1Application.java
@@ -0,0 +1,13 @@
+package com.devsuperiorcapitulo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevsuperiorCapitulo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevsuperiorCapitulo1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperiorcapitulo1/DevsuperiorCapitulo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperiorcapitulo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevsuperiorCapitulo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
